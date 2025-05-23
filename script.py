263405a12885954968f167f7fa87ae3c4984b3cb
unknown <sabinpanta11@gmail.com>
2025-05-23 15:16:54 +0545

    added asyncio

--- script.py
+++ script.py
@@ -2,18 +2,24 @@ import asyncio
 from docx import Document
 from googletrans import Translator
 
-async def translate_text(text, language="ne") -> str:
-    async with Translator() as translator:
-        result = await translator.translate(text, dest=language)
-    return result.text
 
-doc = Document("test.docx")
+async def translate_run(run, translator, language="ne"):
+    translated = await translator.translate(run.text, dest=language)
+    run.text = translated.text
+    run.font.name = "Annapurna SIL"
 
-for paragraph in doc.paragraphs:
-    # run object represent text with styles
 
-    for run in paragraph.runs:
-        run.text = asyncio.run(translate_text(run.text))
-        run.font.name = "Annapurna SIL"
+async def translate_document(doc_path, output_path, language="ne"):
+    doc = Document(doc_path)
+    translator = Translator()
+    tasks = []
+    for paragraph in doc.paragraphs:
+        # run object represent text with styles
+        for run in paragraph.runs:
+            tasks.append(translate_run(run, translator, language))
 
-doc.save("final_translation.docx")
+    await asyncio.gather(*tasks)
+    doc.save(output_path)
+
+
+asyncio.run(translate_document("test.docx", "final.docx"))
